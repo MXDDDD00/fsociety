e2ff2e0f3fd710467edb325feb2b6dba15733ed0
MXDDDD00 <damagemusic005@gmail.com>
2026-04-01 04:12:32 +0300

    Update fsociety.py

--- fsociety.py
+++ fsociety.py
@@ -33,18 +33,17 @@ MY_ART = r"""
 """
 
 def main():
-    art_lines = MY_ART.splitlines()
+    art_lines = MY_ART.strip("\n").split("\n")
     art_width = max(len(line) for line in art_lines)
     spacing = 10
     
-    sys.stdout.write("\033[2J\033[H")
+    sys.stdout.write("\033[?25l\033[2J\033[H")
     
     try:
         offset = 0
         while True:
             size = os.get_terminal_size()
-            term_height = size.lines
-            term_width = size.columns
+            term_height, term_width = size.lines, size.columns
             
             sys.stdout.write("\033[H")
             output = []
@@ -52,7 +51,6 @@ def main():
             for i in range(term_height):
                 line_idx = (i + offset) % len(art_lines)
                 base_line = art_lines[line_idx]
-                
                 full_line = (base_line + " " * spacing) * ((term_width // (art_width + spacing)) + 2)
                 output.append(full_line[:term_width] + "\033[K")
             
@@ -63,7 +61,7 @@ def main():
             time.sleep(0.03)
             
     except KeyboardInterrupt:
-        sys.stdout.write("\033[2J\033[H")
+        sys.stdout.write("\033[?25h\033[2J\033[H")
         sys.stdout.flush()
         sys.exit()
 
